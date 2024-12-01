ba102792a91ea05bc00727aa1b7bb96b26fdb3b6
Robin Jonsson <hejrobinjonsson@gmail.com>
2024-12-01 20:34:45 +0100

    variable name change

--- AoC2024/01/solution.fs
+++ AoC2024/01/solution.fs
@@ -5,19 +5,19 @@ open System.IO
 let run = 
     let lines = File.ReadAllLines("01/example.txt") |> Array.toList
     
-    let parsedIntArray = lines
-                         |> List.map(fun x -> x.Split("   ") |> Array.toList)
-                         |> List.concat
-                         |> List.filter(fun x -> x.Length > 0)
-                         |> List.map(int)
+    let parsedIntList = lines
+                        |> List.map(fun x -> x.Split("   ") |> Array.toList)
+                        |> List.concat
+                        |> List.filter(fun x -> x.Length > 0)
+                        |> List.map(int)
                 
     let filterIndexAndSort x condition = x
                                          |> List.mapi(fun i x -> if condition i then -1 else x)
                                          |> List.filter((<>) -1)
                                          |> List.sort
                                
-    let list1 = filterIndexAndSort parsedIntArray (fun x -> x % 2 = 0)
-    let list2 = filterIndexAndSort parsedIntArray (fun x -> x % 2 = 1)
+    let list1 = filterIndexAndSort parsedIntList (fun x -> x % 2 = 0)
+    let list2 = filterIndexAndSort parsedIntList (fun x -> x % 2 = 1)
     
     let sum = (list1, list2)
               ||> List.map2(fun x y -> abs(x - y))
@@ -31,3 +31,4 @@ let run =
                      |> List.sum
     
     printfn $"Part 2: %d{occurances}"
+    
