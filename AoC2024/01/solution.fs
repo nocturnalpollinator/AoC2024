2e91c7765f71f454fb8caccd1ba5293b395addd7
Robin Jonsson <hejrobinjonsson@gmail.com>
2024-12-01 20:31:12 +0100

    We're doing lists now

--- AoC2024/01/solution.fs
+++ AoC2024/01/solution.fs
@@ -3,31 +3,31 @@ module AoC2024._01.solution
 open System.IO
 
 let run = 
-    let lines = File.ReadAllLines("01/example.txt")
+    let lines = File.ReadAllLines("01/example.txt") |> Array.toList
     
     let parsedIntArray = lines
-                         |> Array.map( _.Split(" ") )
-                         |> Array.concat
-                         |> Array.filter(fun x -> x.Length > 0)
-                         |> Array.map(fun x -> x |> int)
+                         |> List.map(fun x -> x.Split("   ") |> Array.toList)
+                         |> List.concat
+                         |> List.filter(fun x -> x.Length > 0)
+                         |> List.map(int)
                 
     let filterIndexAndSort x condition = x
-                                         |> Array.mapi(fun i x -> if condition i then -1 else x)
-                                         |> Array.filter(fun x -> x <> -1)
-                                         |> Array.sort
+                                         |> List.mapi(fun i x -> if condition i then -1 else x)
+                                         |> List.filter((<>) -1)
+                                         |> List.sort
                                
-    let arr1 = filterIndexAndSort parsedIntArray (fun x -> x % 2 = 0)
-    let arr2 = filterIndexAndSort parsedIntArray (fun x -> x % 2 = 1)
+    let list1 = filterIndexAndSort parsedIntArray (fun x -> x % 2 = 0)
+    let list2 = filterIndexAndSort parsedIntArray (fun x -> x % 2 = 1)
     
-    let sum = (arr1, arr2)
-              ||> Array.map2(fun x y -> abs(x - y))
-              |> Array.sum
+    let sum = (list1, list2)
+              ||> List.map2(fun x y -> abs(x - y))
+              |> List.sum
     
     printfn $"Part 1: %d{sum}"
     
-    let occurances = arr1
-                     |> Array.map(fun x ->
-                         (arr2 |> Seq.filter ((=) x) |> Seq.length ) * x)
-                     |> Array.sum
+    let occurances = list1
+                     |> List.map(fun x ->
+                         (list2 |> List.filter ((=) x) |> List.length ) * x)
+                     |> List.sum
     
     printfn $"Part 2: %d{occurances}"
